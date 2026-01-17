78413308fd78e4c2ee968770e99edc1fa394702f
Oreofe-ux <oreofefaith840@gmail.com>
2026-01-17 05:50:45 +0100

    Update grid-display.js

--- grid-display.js
+++ grid-display.js
@@ -1,3 +1,26 @@
-const image = document.getElementById("artImage");
-const moda
+// declaring constants
+const galleryImages = document.querySelectorAll("artImage");
+const modalImage = document.getElementById("modalImage");
+const modalTitle = document.getElementById("modalTitle");
+const modalArtist = document.getElementById("modalArtist");
+const modalDateDisplay = document.getElementById("modalDate");
+const modalDescription = document.getElementById("modalDescription");
+
+// fetching data from art API
+fetch("https://api.artic.edu/api/v1/artworks?limit=24&fields=id,title,artist_display,date_display,image_id,description")
+.then(res => res.json)
+.then(result => {
+  const artworks = result.data;
+
+  galleryImages.forEach((img, index) => {
+    const artwork = artworks[index];
+    if (!artwork || !artwork.image_id){return;}
+
+    const imageUrl = `https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`;
+    img.src = imageUrl;
+
+    
+  }
+})
+
 
